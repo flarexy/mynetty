bc798d6a3507cd4e6a5e7058a4179a1d0b7a05db
xieyi <670667585@qq.com>
2019-01-14 15:45:04 +0800

    获取属性

--- src/main/java/com/flare/netty/server/NettyServer.java
+++ src/main/java/com/flare/netty/server/NettyServer.java
@@ -3,11 +3,15 @@ package com.flare.netty.server;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.AttributeKey;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 /**
  * @ClassName NettyServer
@@ -18,23 +22,71 @@ import io.netty.handler.codec.string.StringDecoder;
 public class NettyServer {
 
     public static void main(String[] args) {
+        //1.创建引导类 serverBootstrap
+        //2.指定线程模型 channel  - NioServerSocketChannel.class  NIO
+        //3.连接读写处理逻辑 childHandler - ChannelInitializer
+        //4.绑定端口
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         NioEventLoopGroup bossGroup = new NioEventLoopGroup();
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        AttributeKey attributeKey = AttributeKey.newInstance("clientKey");
         serverBootstrap
-                .group(bossGroup,workerGroup)
+                .group(bossGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                // 表示系统用于临时存放已完成三次握手的请求的队列的最大长度，如果连接建立频繁，服务器处理创建新连接较慢，可以适当调大这个参数
+                .option(ChannelOption.SO_BACKLOG,1024)
+                // 给每条连接指定属性
+                .childAttr(attributeKey,"clientValue")
+                // 开启TCP底层心跳机制，true为开启
+                .childOption(ChannelOption.SO_KEEPALIVE,true)
+                // 是否开启Nagle算法，true表示关闭，false表示开启，通俗地说，如果要求高实时性，有数据发送时就马上发送，就关闭，
+                // 如果需要减少发送次数减少网络交互，就开启
+                .childOption(ChannelOption.TCP_NODELAY,true)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel nioSocketChannel) {
+                        // 获取属性
+                        System.out.println(nioSocketChannel.attr(attributeKey).get());
                         nioSocketChannel.pipeline().addLast(new StringDecoder());
                         nioSocketChannel.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
                             @Override
                             protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+                                // 新消息处理
                                 System.out.println(s);
                             }
                         });
                     }
-                }).bind(8000);
+                });
+
+        // 服务端启动执行
+        // 正常不使用
+        serverBootstrap.handler(new ChannelInitializer<NioServerSocketChannel>() {
+            @Override
+            protected void initChannel(NioServerSocketChannel nioServerSocketChannel) throws Exception {
+                System.out.println("服务端启动中");
+            }
+        });
+        // 绑定端口
+        bind(serverBootstrap,8000);
+    }
+
+    /**
+     * 绑定端口
+     * @param serverBootstrap
+     * @param port
+     */
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            @Override
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if (future.isSuccess()) {
+                    System.out.println("端口[" + port + "]绑定成功");
+                } else {
+                    // 如果当前端口绑定失败，则端口+1重新绑定
+                    System.out.println("端口[" + port + "]绑定失败");
+                    bind(serverBootstrap, port + 1);
+                }
+            }
+        });
     }
 }
